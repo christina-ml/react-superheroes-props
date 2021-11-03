284834115f071210cbf3908c586d51f70ca849e7
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-03 11:23:36 -0400

    added SpiderMan.js component, and imported into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import superHeroes from './data/superHeroes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1 id="title">Marvel Superheroes!</h1>
+      <button>Add a Superhero!</button>
     </div>
   );
 }
---
+++ src/components/SpiderMan.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+class SpiderMan extends Component{
+
+}
+
+export default SpiderMan;
