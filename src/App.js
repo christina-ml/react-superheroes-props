aef5da22381653d740d863ad3d81593eb6aa654b
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-03 12:37:18 -0400

    added allHeroes to the HeroList.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import superHeroes from './data/superHeroes';
-import SpiderMan from './components/SpiderMan';
-import IronMan from './components/IronMan';
-
-// import HeroList from './components/HeroList';
-import HeroCard from './components/HeroCard';
+import HeroList from './components/HeroList';
 import './App.css';
 
 function App() {
-  let allHeroes = superHeroes.map((hero, i) => {
-    return (<HeroCard key={hero.name + i} hero={hero} />)
-  })
-
-  console.log(allHeroes);
-
   return (
     <div className="App">
       <h1 id="title">Marvel Superheroes!</h1>
       <button>Add a Superhero!</button>
       <div id="heroesContainer">
-        {/* <SpiderMan spiderMan={superHeroes[0]}/>
-        <IronMan ironMan={superHeroes[6]}/> */}
-        {allHeroes}
+        <HeroList />
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+/* No longer using these in App.js: */
+// import superHeroes from './data/superHeroes';
+// import SpiderMan from './components/SpiderMan';
+// import IronMan from './components/IronMan';
+// import HeroCard from './components/HeroCard';
+
+{/* <SpiderMan spiderMan={superHeroes[0]}/>
+<IronMan ironMan={superHeroes[6]}/> */}
--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -1,13 +1,21 @@
 import { Component } from "react";
 import HeroCard from "./HeroCard";
+import superHeroes from "../data/superHeroes";
 
 class HeroList extends Component{
     constructor(){
         super();
     }
-
+  
     render(){
-        return null;
+        let allHeroes = superHeroes.map((hero, i) => {
+            return (<HeroCard key={hero.name + i} hero={hero} />)
+        })
+        return(
+            <div id="heroesContainer">
+                { allHeroes }
+            </div>
+        );
     }
 }
 
